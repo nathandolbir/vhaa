052aa2c4d654801badebad415a12b9cb457ec640
nathandolbir <75330713+nathandolbir@users.noreply.github.com>
2022-02-14 15:44:03 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>⚛MediBot💬</h1>
+      </header>
+      <section className="body"></section>
+    </div>
+  );
+}
+
+export default App;
