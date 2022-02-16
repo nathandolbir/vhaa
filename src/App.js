cea72f1a608b31428d429d9a86ec105d81934648
nathandolbir <75330713+nathandolbir@users.noreply.github.com>
2022-02-16 17:36:26 -0500

    Implemented Google auth and DS popup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
+import upload from "./pictures/upload.png";
 
-// Import the functions you need from the SDKs you need
+import React, { useEffect, useRef, useState } from "react";
+/* FUCK THIS SHIT 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import "firebase/compat/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { getStorage, ref } from "firebase/storage";
+*/
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/analytics";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyARA0qKiir3MBYbAY1UNEBc31B79Hkdp7Q",
   authDomain: "virtual-health-assistant-aiisc.firebaseapp.com",
@@ -20,15 +35,67 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const firebaseApp = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const firestore = firebase.firestore(); // used to access firestore doc-DB
+
+// const db = getFirestore(firebaseApp);
+// Create a root reference
+// const storage = getStorage();
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
       <header className="App-header">
         <h1>⚛MediBot💬</h1>
+        <SignOut />
       </header>
-      <section className="body"></section>
+      <section className="body">
+        {user ? (
+          <section className="main-page">
+            <DSupload></DSupload>
+          </section>
+        ) : (
+          <SignIn />
+        )}
+      </section>
+    </div>
+  );
+}
+// console.log(firebaseApp.auth.GoogleAuthProvider());
+function SignIn() {
+  const signInWithGoogle = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <div style={{ alignItems: "center" }}>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+    </div>
+  );
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+}
+
+function DSupload() {
+  return (
+    <div className="dsupload">
+      <p>Hello! Please upload your Discharge Summary to Medibot.</p>
+      <div className="upload-box">
+        <img className="upload" src={upload}></img>
+      </div>
     </div>
   );
 }
